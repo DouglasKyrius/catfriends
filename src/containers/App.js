7792f75b85307a0fea591008c01fc8c97e4827a9
DouglasKyrius <douglaskyrius@gmail.com>
2020-05-27 22:07:05 -0400

    removing scroll div

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends Component {
@@ -28,15 +27,14 @@ class App extends Component {
     const filteredRobots = robots.filter(robot =>{
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
+
     return !robots.length ?
       <h1>Loading</h1> :
       (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
+          <CardList robots={filteredRobots} />
         </div>
       );
   }
