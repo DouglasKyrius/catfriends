3f68e17376cd4e6138ac5c8678781b7d28b7f50b
DouglasKyrius <douglaskyrius@gmail.com>
2020-06-02 21:37:02 -0400

    fixing issues

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { createLogger } from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import { searchRobots } from './reducers';
 import 'tachyons';
 
-const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(thunkMiddleware, logger));
+const store = createStore(searchRobots);
 
 ReactDOM.render(
   <React.StrictMode>
